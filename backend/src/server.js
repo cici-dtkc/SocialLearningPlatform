b0e9672b551d3fad616e44b5480ce6099ac3d223
Đỗ Thị Kim Chi <23130030@st.hcmuaf.edu.vn>
2026-07-16 22:37:37 +0700

    update cấu hình

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,8 @@ const http = require("http");
 const app = require("./app");
 
 const server = http.createServer(app);
+const port = process.env.PORT || 5000;
 
-server.listen(process.env.PORT, () => {
+server.listen(port, () => {
     console.log(`Server running`);
 });
